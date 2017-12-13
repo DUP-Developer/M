7ae7f7d6d9306103a851b286d67583f9e2d0cdb6
joerverson santos <joerverson.santos@gmail.com>
2017-12-13 12:25:05 -0300

    em tese rodando as na web

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
     res.status(404).send("I'm sorry, not know ^~^");
 })
-
+console.log(process);
 
 //ouvindo os serviços
-http.listen(3000, () => console.log('Im listen... man!!!'));
+http.listen(process.env.PORT || 8080, () => console.log('Im listen... man!!!'));
